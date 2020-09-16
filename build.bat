b9f861f2c12a2c7d4a2b6e0a3bccf4fd8efc8ab9
jonathan <colombiano.92@live.com>
2020-09-16 16:21:11 -0500

    initial commit

---
+++ build.bat
@@ -0,0 +1,10 @@
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o sprites.o "sprites/sprites.c"
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o bkgSprites.o "sprites/bkgSprites.c"
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
+C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wl-yp0x143=0x80 -o main.gb sprites.o bkgSprites.o main.o
+copy main.gb "C:\Program Files\BGB\main.gb" /y 
+
+del /f /q /s *.lst
+del /f /q /s *.o
+del /f /q /s *.map
+del /f /q /s *.sym
