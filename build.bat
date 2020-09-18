5dada10fa2c120be04a3d27819bf544c9323bfbb
jonathan <colombiano.92@live.com>
2020-09-18 05:30:54 -0500

    stuff

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o sprites.o "sprites/spr
 C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o bkgSprites.o "sprites/bkgSprites.c"
 C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -DUSE_SFR_FOR_REG -c -o main.o main.c
 C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wl-yp0x143=0x80 -o main.gb sprites.o bkgSprites.o main.o
-copy main.gb "C:\Program Files\BGB\main.gb" /y 
+copy main.gb "C:\Users\jonathan\Documents\BGB\main.gb" /y 
 
 del /f /q /s *.lst
 del /f /q /s *.o
